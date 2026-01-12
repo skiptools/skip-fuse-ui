d3aebf3a1728321104356a036b3b8245c046d459
Marc Prud'hommeaux <marc@skip.tools>
2026-01-11 20:26:49 -0500

    Update skip-ui dependency to 1.46.0

--- Package.swift
+++ Package.swift
@@ -16,9 +16,7 @@ let package = Package(
         .package(url: "https://source.skip.tools/skip-bridge.git", "0.16.4"..<"2.0.0"),
         .package(url: "https://source.skip.tools/skip-android-bridge.git", "0.6.1"..<"2.0.0"),
         .package(url: "https://source.skip.tools/swift-jni.git", "0.3.1"..<"2.0.0"),
-        // TODO: add back once merging https://github.com/skiptools/skip-ui/pull/294
-        //.package(url: "https://source.skip.tools/skip-ui.git", from: "1.45.0") 
-        .package(url: "https://github.com/vincentborko/skip-ui.git", branch: "new-modifiers")
+        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.46.0") 
     ],
     targets: [
         .target(name: "SkipFuseUI", dependencies: ["SkipSwiftUI"]),
