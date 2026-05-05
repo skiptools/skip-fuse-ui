e35660bdf5214e11e3f34a926f8324c00031aad4
Tyler Jones <tylerjonesio@users.noreply.github.com>
2026-05-05 15:54:58 -0600

    Fix .navigationBarHidden modifier (#99)

--- Sources/SkipSwiftUI/Containers/Navigation.swift
+++ Sources/SkipSwiftUI/Containers/Navigation.swift
@@ -551,7 +551,7 @@ extension View {
     }
 
     nonisolated public func navigationBarHidden(_ hidden: Bool) -> some View {
-        return toolbarVisibility(.hidden, for: .navigationBar)
+        return toolbarVisibility(hidden ? .hidden : .visible, for: .navigationBar)
     }
 }
 
