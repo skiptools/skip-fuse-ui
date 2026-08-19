7c772c2457216ad5826bf2274f1ec7f73c87b1f6
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-08-19 13:29:42 -0400

    Change source.skip.tools references to github.com/skiptools

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
         .library(name: "SkipSwiftUISamples", type: .dynamic, targets: ["SkipSwiftUISamples"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.9.4"),
-        .package(url: "https://source.skip.tools/skip-fuse.git", from: "1.0.2"),
-        .package(url: "https://source.skip.tools/skip-bridge.git", "0.17.2"..<"2.0.0"),
-        .package(url: "https://source.skip.tools/skip-android-bridge.git", "0.6.4"..<"2.0.0"),
-        .package(url: "https://source.skip.tools/swift-jni.git", "0.5.0"..<"2.0.0"),
-        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.59.0"),
+        .package(url: "https://github.com/skiptools/skip.git", from: "1.9.4"),
+        .package(url: "https://github.com/skiptools/skip-fuse.git", from: "1.0.2"),
+        .package(url: "https://github.com/skiptools/skip-bridge.git", "0.17.2"..<"2.0.0"),
+        .package(url: "https://github.com/skiptools/skip-android-bridge.git", "0.6.4"..<"2.0.0"),
+        .package(url: "https://github.com/skiptools/swift-jni.git", "0.5.0"..<"2.0.0"),
+        .package(url: "https://github.com/skiptools/skip-ui.git", from: "1.59.0"),
     ],
     targets: [
         .target(name: "SkipFuseUI", dependencies: ["SkipSwiftUI"]),
